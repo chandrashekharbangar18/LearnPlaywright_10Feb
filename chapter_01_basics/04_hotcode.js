a4507b02e25cd656c53d77126287544cd5b1ad93
Dev <chandrashekharbangar18@gmail.com>
2026-02-12 15:56:31 +0530

    Initial commit

---
+++ chapter_01_basics/04_hotcode.js
@@ -0,0 +1,17 @@
+console.log('Hello !!!');  // 
+
+
+// Hot code
+
+// Ex1: Add two numbers
+function add(a, b) 
+{
+    return a + b;
+}
+console.log("Addition is:", add(10, 20));
+
+// Ex2: loop
+for(let i=1; i<=5; i++)
+{ 
+    console.log(i);
+}
