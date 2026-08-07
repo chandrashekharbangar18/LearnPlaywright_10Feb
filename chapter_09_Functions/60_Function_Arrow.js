0c27c35f43f6101410658c49d0dc1de60fdc1213
Dev <chandrashekharbangar18@gmail.com>
2026-08-07 10:52:36 +0530

    Add arrow function examples

---
+++ chapter_09_Functions/60_Function_Arrow.js
@@ -0,0 +1,30 @@
+// Arrow Functions -->
+// If you want to make normal function to arrow function -->
+// remove the keyword function, return, curly braces and use equal to arrow like = >
+
+// Ex1
+// Normal funtion
+const greet = function (fName)
+{
+    return `Hello, ${fName}!`;
+}
+
+console.log(greet("Chandra"));
+
+// Arrow fuction for above example is -->
+
+const greet1 = (fName1) => `Hello, ${fName1}!`;
+console.log(greet1("Chandra"));
+
+// No params
+const getEnv = () => "staging";
+console.log(getEnv());
+
+// Ex
+
+const getResult = (score) => {
+    if (score > 40) return "pass"
+    else "fail";
+}
+
+console.log(getResult(39));
